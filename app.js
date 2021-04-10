abf8d151940ba11e6eb25f36157dcaa1a3f1781a
Praise <Odohpraise35@gmail.com>
2021-04-10 22:21:27 +0100

    git commit -m

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
@@ -19,3 +20,27 @@ const navSlide = () => {
     });
 }
 navSlide();
+=======
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', () => {
+        //Toggle
+        nav.classList.toggle('nav-active');
+        //Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`
+            }
+        });
+        //burger Animation
+     burger.classList.toggle('toggle');
+    });
+}
+navSlide();
+
+>>>>>>> 3a8febc (modified files)
